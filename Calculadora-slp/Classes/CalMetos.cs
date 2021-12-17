3595e427c93a79184b6157f1f926678a3142e1c0
Gilberto Martins <gilbertomartins643@gmail.com>
2021-12-17 15:34:21 -0300

    Complit

---
+++ Calculadora-slp/Classes/CalMetos.cs
@@ -0,0 +1,25 @@
+namespace Calculadora_slp.Classes
+{
+    public class CalMetos
+    {
+        public double Soma ( double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Mult (double x, double y)
+        {
+            return x * y;
+        }
+
+         public double Divi (double x, double y)
+        {
+            return x / y;
+        }
+
+         public double Sub (double x, double y)
+        {
+            return x - y;
+        }
+    }
+}
--- Calculadora-slp/Program.cs
+++ Calculadora-slp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Calculadora_slp.Classes;
 
 namespace Calculadora_slp
 {
@@ -6,7 +7,62 @@ namespace Calculadora_slp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CalMetos op = new CalMetos();
+
+            Console.Clear();
+            Console.WriteLine("--Calculadora--");
+
+            Console.WriteLine("Qual operação deseja realizar e em sequida ensira os valores da operação");
+            Console.WriteLine("1 Para Somar, 2 Para Subtrair, 3 Para Multiplicar e 4 Para Dividir");
+
+            int ope;
+            double x , y;
+            bool continuar;
+            char valid;
+
+            do
+            {
+                switch (ope = Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        x = Convert.ToDouble(Console.ReadLine());
+                        y = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(op.Soma(x, y));
+                        break;
+
+                    case 2:
+                        x = Convert.ToDouble(Console.ReadLine());
+                        y = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(op.Sub(x , y));
+                        break;
+
+                    case 3:
+                        x = Convert.ToDouble(Console.ReadLine());
+                        y = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(op.Mult(x, y));
+                        break;
+
+                    case 4:
+                        x = Convert.ToDouble(Console.ReadLine());
+                        y = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine(op.Divi(x, y));
+                        break;
+
+                    default:
+                        Console.WriteLine("--Operação não encontrada--");
+                        break;
+                }
+                Console.WriteLine("Deseja repetir a operação? S / N");
+                valid = Convert.ToChar(Console.ReadLine());
+                if(valid == 'S')
+                {
+                    continuar = true;
+                }
+                else
+                {
+                    continuar = false;
+                }
+            }while(continuar);
         }
     }
 }
