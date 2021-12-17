8fb7deca335723790e071befd92af7e1bbe0f76c
Gilberto Martins <gilbertomartins643@gmail.com>
2021-12-17 15:05:27 -0300

    init

---
+++ Calculadora-slp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculadora_slp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
